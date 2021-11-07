f348f1a815620723f11412d70fac0902982b4917
casdidier <cheung.didier@gmail.com>
2021-11-07 17:40:11 +0100

    move data to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,12 +2,23 @@ import React, { ReactElement } from 'react';
 
 import Selector from './Selector';
 
-interface Props {}
+const STOPS = [
+  'Le Marais',
+  'Saint-Germain-des-Prés',
+  'Le quartier latin',
+  'Les Tuileries et le Louvre',
+  'Opéra',
+  'Champs-Élysées',
+  'Montmartre',
+  'Invalides-tour Eiffel',
+  'Belleville-Ménilmontant',
+  'Bastille',
+] as const;
 
-function Container({}: Props): ReactElement {
+function Container(): ReactElement {
   return (
     <div>
-      <Selector />
+      <Selector stopList={STOPS} />
     </div>
   );
 }
--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,27 +1,16 @@
 import React, { ReactElement } from 'react';
 import { Form } from 'react-bootstrap';
 
-interface Props {}
+interface Props {
+  stopList: readonly string[];
+}
 
-const STOPS = [
-  'Le Marais',
-  'Saint-Germain-des-Prés',
-  'Le quartier latin',
-  'Les Tuileries et le Louvre',
-  'Opéra',
-  'Champs-Élysées',
-  'Montmartre',
-  'Invalides-tour Eiffel',
-  'Belleville-Ménilmontant',
-  'Bastille',
-] as const;
-
-function Selector({}: Props): ReactElement {
+function Selector({ stopList }: Props): ReactElement {
   return (
     <div>
       <Form.Select aria-label="Default select example">
         <option>Select your start stop</option>
-        {STOPS.map((stop, index) => {
+        {stopList.map((stop, index) => {
           return (
             <option value={stop} key={index}>
               {stop}
